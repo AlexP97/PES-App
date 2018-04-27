a714cea8dc4a59120eaa5bbae943833a8f98358b
Mar Badia <marbadia97@gmail.com>
2018-04-27 12:25:56 +0200

    Main menu doneeeee

--- app/src/main/java/com/example/daniel/assistme/MenuActivity.java
+++ app/src/main/java/com/example/daniel/assistme/MenuActivity.java
@@ -1,20 +1,29 @@
 package com.example.daniel.assistme;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MenuActivity extends AppCompatActivity {
 
     String username;
+    Context context;
+
+    protected void onCreate(Bundle savedInstanceState) {
+
+        username = getIntent().getStringExtra("EXTRA_SESSION_ID");
+        context = getApplicationContext();
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu){
-
-        username = getIntent().getStringExtra("EXTRA_SESSION_ID");
 
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
@@ -31,5 +40,25 @@ public class MenuActivity extends AppCompatActivity {
                 break;
         }
         return true;
+    }
+
+    public void ChatButton(View view) {
+        Intent intent = new Intent(context, EditActivity.class);
+        startActivity(intent);
+    }
+
+    public void ForumButton(View view) {
+        Intent intent = new Intent(context, EditActivity.class);
+        startActivity(intent);
+    }
+
+    public void GuidesButton(View view) {
+        Intent intent = new Intent(context, EditActivity.class);
+        startActivity(intent);
+    }
+
+    public void NewsButton(View view) {
+        Intent intent = new Intent(context, EditActivity.class);
+        startActivity(intent);
     }
 }
