d1ae73139a9d68ad0194422aee8200d2548f10b5
Dani Martinez <dissidia_1247@hotmail.es>
2018-05-10 20:44:10 +0200

    GooglePLay Services y Firebase

--- app/src/main/java/com/example/daniel/assistme/ChatActivity.java
+++ app/src/main/java/com/example/daniel/assistme/ChatActivity.java
@@ -2,12 +2,30 @@ package com.example.daniel.assistme;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class ChatActivity extends AppCompatActivity {
+
+    CircleImageView userImage;
+    TextView userName;
+    RecyclerView messageList;
+    EditText messageText;
+    Button sendButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+
+        userImage = (CircleImageView) findViewById(R.id.fotoPerfil);
+        userName = (TextView) findViewById(R.id.userName);
+        messageList = (RecyclerView) findViewById(R.id.messageList);
+        messageText = (EditText) findViewById(R.id.messageText);
+        sendButton = (Button) findViewById(R.id.sendButton);
     }
 }
