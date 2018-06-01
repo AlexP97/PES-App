98154f71a4225ee69b487cf562782c2699f9ef81
Elias Bennour <eliasbenur@hotmail.com>
2018-06-01 13:45:48 +0200

    Version beta del traductor de guias

--- app/src/main/java/com/example/daniel/assistme/ViewGuideActivity.java
+++ app/src/main/java/com/example/daniel/assistme/ViewGuideActivity.java
@@ -1,5 +1,6 @@
 package com.example.daniel.assistme;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Html;
@@ -9,6 +10,15 @@ import org.json.JSONObject;
 import android.util.Log;
 import android.widget.Button;
 import android.view.View;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
 
 
 public class ViewGuideActivity extends AppCompatActivity {
@@ -50,5 +60,90 @@ public class ViewGuideActivity extends AppCompatActivity {
             intent.putExtra("points", guidePoints);
             startActivity(intent);
         }
+    }
+
+    public void translate(android.view.View view) throws UnsupportedEncodingException {
+        Toast t = Toast.makeText(getApplicationContext(), "Translate!!", Toast.LENGTH_SHORT);
+        t.show();
+
+
+        //Poner la peticion http aqui
+        ViewGuideActivity.AsyncTrans asyncTrans = new ViewGuideActivity.AsyncTrans();
+        asyncTrans.execute("");
+    }
+
+    private class AsyncTrans extends AsyncTask<String, Void, String> {
+
+
+        @Override
+        protected String doInBackground(String... data) {
+            BufferedReader reader=null;
+
+            try {
+                String mKey = "trnsl.1.1.20180601T105748Z.dc0633591153717a.53d870f1d138ff601a4850c3894c477aa44c814f";
+                String sourceText = "My name is Elias";
+                String sourceLang = "en";
+                String destinationLang = "es";
+                //URL url = new URL("https://translate.yandex.net/api/v1.5/tr.json/translate?lang=en-ru&key=trnsl.1.1.20180601T105748Z.dc0633591153717a.53d870f1d138ff601a4850c3894c477aa44c814f");
+                String yandexUrl = "https://translate.yandex.net/api/v1.5/tr.json/translate?key=" + mKey
+                        + "&text=" + sourceText + "&lang=" + sourceLang + "-" + destinationLang;
+                URL url = new URL(yandexUrl);
+                // Send POST data request
+
+                URLConnection conn = url.openConnection();
+                conn.setDoOutput(true);
+                OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
+                String d = "";
+                for (int i = 0; i < data.length; ++i) d += data[i];
+                wr.write( d );
+                wr.flush();
+                Log.e("data", d);
+
+                // Get the server response
+
+                reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                StringBuilder sb = new StringBuilder();
+                String line = null;
+
+                // Read Server Response
+                while((line = reader.readLine()) != null)
+                {
+                    // Append server response in string
+                    sb.append(line + "\n");
+                }
+
+                String jsonString = sb.toString();
+                Log.e("result-translate", jsonString);
+                return jsonString;
+            }
+            catch(Exception ex) {
+
+            }
+            finally
+            {
+                try
+                {
+                    reader.close();
+                }
+
+                catch(Exception ex) {
+
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            try {
+                Toast t = null;
+                t = Toast.makeText(getApplicationContext(), result, Toast.LENGTH_SHORT);
+                t.show();
+
+                super.onPostExecute(result);
+            }
+            catch(Exception e){}
+        }
+
     }
 }
