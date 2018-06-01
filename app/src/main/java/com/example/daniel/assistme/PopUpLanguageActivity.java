1a6c85b175b6f25409dd7e95ca176379768e2203
Busta <busvar@localhost.localdomain>
2018-06-01 19:13:18 +0200

    Gestion de idiomas done. Hay que mirar bien como gestionamos la pila de activities...

--- app/src/main/java/com/example/daniel/assistme/PopUpLanguageActivity.java
+++ app/src/main/java/com/example/daniel/assistme/PopUpLanguageActivity.java
@@ -1,11 +1,18 @@
 package com.example.daniel.assistme;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.View;
 import android.widget.RadioGroup;
 import android.widget.Toast;
+import java.util.Locale;
+
+import android.content.Intent;
+import android.content.res.Configuration;
+
 
 public class PopUpLanguageActivity extends AppCompatActivity {
 
@@ -27,18 +34,33 @@ public class PopUpLanguageActivity extends AppCompatActivity {
 
         RadioGroup rg = findViewById(R.id.LanguageGroup);
         if (rg.getCheckedRadioButtonId() == R.id.radioButton3) {
+            setLocale("eng");
             Toast t = Toast.makeText(getApplicationContext(), "Language set to English", Toast.LENGTH_SHORT);
             t.show();
         }
         else if (rg.getCheckedRadioButtonId() == R.id.radioButton2) {
+            setLocale("cat");
             Toast t = Toast.makeText(getApplicationContext(), "Idioma canviat al Català", Toast.LENGTH_SHORT);
             t.show();
         }
         else if (rg.getCheckedRadioButtonId() == R.id.radioButton) {
+            setLocale("spa");
             Toast t = Toast.makeText(getApplicationContext(), "Idioma cambiado al Español", Toast.LENGTH_SHORT);
             t.show();
         }
-
+        super.recreate();
         finish();
     }
+
+    private void setLocale(String lang) {
+        Locale locale = new Locale(lang);
+        Locale.setDefault(locale);
+        Configuration config = getBaseContext().getResources().getConfiguration();
+        config.locale = locale;
+        getBaseContext().getResources().updateConfiguration(config,
+                getBaseContext().getResources().getDisplayMetrics());
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
 }
